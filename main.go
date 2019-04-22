bd896862da5665ab255b099e58a77d8fe5263a04
Bruce Semple <sempleb@us.ibm.com>
2019-04-22 18:04:26 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // Respond to call on endpoint
 func respond(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello Bruce from your Go sample application 03 running in Microclimate! (edited on GitHub")
+	io.WriteString(w, "Hello Bruce from your Go sample application 03 running in Microclimate! (edited on GitHub!!!!)")
 	fmt.Printf("main.go: Go app endpoint response\n")
 }
 
